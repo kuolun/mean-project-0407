32ca8ae146d904d73c4ce342cf2d12a69603d431
kuolun <kuolun@gmail.com>
2017-04-08 00:39:06 +0800

    save user to db

--- app/routes.js
+++ app/routes.js
@@ -4,66 +4,68 @@ var router = express.Router();
 // Models
 var Category = require('./models/category');
 var Product = require('./models/product');
+//取得user model
+var User = require('./models/user');
 
 /**
  * 目錄相關的routes
  */
 //addCategory
 router.post('/addCategory', function (req, res) {
-    var category = new Category();
-    category.name = req.body.name;
+  var category = new Category();
+  category.name = req.body.name;
 
-    category.save(function (err, category) {
-        if (err) {
-            return console.log(err);
+  category.save(function (err, category) {
+    if (err) {
+      return console.log(err);
 
-        } else {
-            res.json({
-                category: category
-            });
-        }
-    });
+    } else {
+      res.json({
+        category: category
+      });
+    }
+  });
 });
 
 //get all categories
 router.get('/categories', function (req, res) {
-    Category.find({}, function (err, categories) {
-        if (err) {
-            res.status(500).json({
-                error: err.toString()
-            });
-        } else {
-            res.json({
-                categories: categories
-            });
-        }
-    });
+  Category.find({}, function (err, categories) {
+    if (err) {
+      res.status(500).json({
+        error: err.toString()
+      });
+    } else {
+      res.json({
+        categories: categories
+      });
+    }
+  });
 });
 
 // get category with id
 router.get('/category/:id', function (req, res) {
-    Category.findById({
-        _id: req.params.id
-    }, function (err, category) {
-        if (err) {
-            return res.status(500)
-                .json({
-                    error: err.toString()
-                });
-        }
-
-        if (!category) {
-            return res.status(404)
-                .json({
-                    error: 'Not found'
-                });
-        }
-
-        res.json({
-            category: category
+  Category.findById({
+    _id: req.params.id
+  }, function (err, category) {
+    if (err) {
+      return res.status(500)
+        .json({
+          error: err.toString()
         });
+    }
 
-    })
+    if (!category) {
+      return res.status(404)
+        .json({
+          error: 'Not found'
+        });
+    }
+
+    res.json({
+      category: category
+    });
+
+  })
 });
 
 
@@ -73,51 +75,108 @@ router.get('/category/:id', function (req, res) {
 
 //get products with category id
 router.get('/products/:cateid', function (req, res) {
-    Product.find({
-            category: req.params.cateid
-        })
-        .populate('category')
-        .exec(function (err, products) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.json({
-                    products: products
-                });
-            }
+  Product.find({
+      category: req.params.cateid
+    })
+    .populate('category')
+    .exec(function (err, products) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json({
+          products: products
         });
+      }
+    });
 });
 
 //get all products
 router.get('/products', function (req, res) {
-    Product.find({})
-        .populate('category')
-        // .select('-name -price')
-        .exec(function (err, products) {
-            if (err) {
-                res.status(500).
-                json({
-                    error: err.toString()
-                });
-            } else {
-                res.json({
-                    products: products
-                })
-            }
+  Product.find({})
+    .populate('category')
+    // .select('-name -price')
+    .exec(function (err, products) {
+      if (err) {
+        res.status(500).
+        json({
+          error: err.toString()
         });
+      } else {
+        res.json({
+          products: products
+        })
+      }
+    });
 });
 
 
 //get product with id
 router.get('/product/:id', (req, res) => {
-    Product.findById({
-        _id: req.params.id
-    }, (err, product) => {
-        if (err)
-            console.log(err);
-        res.json({
-            product: product
-        });
+  Product.findById({
+    _id: req.params.id
+  }, (err, product) => {
+    if (err)
+      console.log(err);
+    res.json({
+      product: product
+    });
+  });
+});
+
+
+
+/**
+ * check user in DB
+ */
+router.get('/checkDBUser/:email', function (req, res, next) {
+  User.findOne({
+    email: req.params.email
+  }, function (err, user) {
+    if (err) return next(err);
+    res.json({
+      user: user
+    });
+  });
+});
+
+/**
+ * add new user from Auth0
+ */
+router.post('/newUser', function (req, res, next) {
+  console.log(typeof req.body);
+  var profile = req.body;
+  var newUser = new User();
+  //profile是Auth0回傳的資訊
+  newUser.clientID = profile.clientID;
+  newUser.email = profile.email;
+  newUser.profile.username = profile.name;
+  newUser.profile.picture = profile.picture;
+  //把新user存到DB
+  newUser.save(function (err, user) {
+    if (err) {
+      console.log('save error');
+      throw err;
+    }
+    console.log('new user created!');
+    return res.json({
+      savedUser: user
+    });
+  });
+});
+
+// 載入User資料
+router.get('/user/:email', function (req, res, next) {
+  User.findOne({
+      email: req.params.email
+    })
+    //因為product的type為ObjectId所以要populate
+    .populate('data.cart.product')
+    .exec(function (err, user) {
+      console.log('user in DB:', user);
+      if (err) return next(err);
+      res.json({
+        loadedUser: user
+      });
     });
 });
 
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 
 import { tokenNotExpired } from 'angular2-jwt';
 
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Router } from '@angular/router';
+
+
 
 // Avoid name not found warnings
 declare var Auth0Lock;
@@ -9,15 +13,82 @@ declare var Auth0Lock;
 @Injectable()
 export class AuthService {
 
+  userProfile;
 
   lock = new Auth0Lock('WYZtNTzNCFi6UxHwStr8F1HgngOMnI5t', 'kuolun.auth0.com', {});
 
-  constructor() {
+  constructor(private _http: Http, private _router: Router) {
     // Add callback for lock `authenticated` event
     this.lock.on("authenticated", (authResult) => {
-      localStorage.setItem('id_token', authResult.idToken);
+
+      this.lock.getUserInfo(authResult.accessToken, (error, profile) => {
+        if (error) {
+          throw new Error(error);
+        }
+        //檢查是否為登入
+        this.checkDBUser(profile)
+          .subscribe(data => {
+            if (!data.user) {
+              //如果db不存在此user
+              //將Auth0提供的user profile存到DB
+              this.createUser(profile)
+                .subscribe(
+                data => {
+                  localStorage.setItem('id_token', authResult.idToken);
+                  //db回傳的存到localStorage
+                  localStorage.setItem('profile', JSON.stringify(data.savedUser));
+                  this.userProfile = data.savedUser;
+                },
+                err => this.logout());
+            } else {
+              console.log('User already in DB,load user data');
+              this.loadUser(profile)
+                .subscribe(data => {
+                  console.log('LoadedUser:', data.loadedUser);
+                  // 把載入的user存到localStorage
+                  localStorage.setItem('id_token', authResult.idToken);
+                  localStorage.setItem('profile', JSON.stringify(data.loadedUser));
+                  this.userProfile = data.loadedUser;
+                });
+            }
+          });
+      });
     });
   }
+
+  /**
+   * Check User是否存在DB
+   */
+  checkDBUser(profile) {
+    return this._http.get(`http://localhost:3000/api/checkDBUser/${profile.email}`)
+      .map(res => res.json());
+  }
+
+  /**
+   * Create a user
+   */
+  createUser(profile) {
+    console.log("createing user...");
+    // 加上header info
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let bodyString = JSON.stringify(profile);
+
+    //body要轉成string,headers要用物件
+    return this._http.post('http://localhost:3000/api/newUser', bodyString, options)
+      .map(res => res.json());
+  }
+
+  /**
+ * 載入DB User資料
+ * 把productId換成實際product資料
+ */
+  loadUser(profile) {
+    return this._http.get(`http://localhost:3000/api/user/${profile.email}`)
+      .map(res => res.json());
+  }
+
+
 
   public login() {
     // Call the show method to display the widget.
@@ -33,6 +104,9 @@ export class AuthService {
   public logout() {
     // Remove token from localStorage
     localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
+    this.userProfile = null;
+    this._router.navigate(['/']);
   }
 
 }
