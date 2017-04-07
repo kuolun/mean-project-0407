43e16437a894a6034c50ccbba74f67f40b7204d7
kuolun <kuolun@gmail.com>
2017-04-07 16:28:16 +0800

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductlistComponent } from './components/productlist/productlist.component';
+import { ProductdetailComponent } from './components/productdetail/productdetail.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CartComponent } from './components/cart/cart.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ProductlistComponent },
+  { path: 'productdetail', component: ProductdetailComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: '' }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductlistComponent,
+    ProductdetailComponent,
+    ProfileComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
