89082f53d02f88ae156593c3c7bb0b3fb85a1355
kuolun <kuolun@gmail.com>
2017-04-08 09:24:21 +0800

    add profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../shared/services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user;
+
+  constructor(private _auth: AuthService) {
+    this.user = this._auth.userProfile;
+  }
 
   ngOnInit() {
   }
